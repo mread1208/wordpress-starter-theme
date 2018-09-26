c3816ceed45836a95c2348b6f1ecf53bb52e8b5d
Michael Read <michaelread1208@gmail.com>
2018-09-25 22:52:33 -0500

    Removed bootstrap code from searchform.

--- searchform.php
+++ searchform.php
@@ -1,7 +1,5 @@
 <form action="<?php bloginfo('url'); ?>" id="searchform" class="form-inline" method="get">
-    <div class="form-group">
-        <label for="s" class="sr-only">Search for:</label>
-        <input type="text" id="s" class="form-control" name="s" value="" />
-    </div>
+    <label for="s" class="sr-only">Search for:</label>
+    <input type="text" id="s" class="form-control" name="s" value="" />
     <input type="submit" value="Search" id="searchsubmit" class="btn btn-primary" />
 </form>
