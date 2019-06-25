b45fe74a388f3fbc8c20c8ed9422446ffa0dfcc9
Michael Read <michaelread1208@gmail.com>
2019-06-24 22:35:13 -0500

    Mobile menu toggling sub menu

--- js/functions.js
+++ js/functions.js
@@ -2,12 +2,31 @@ var menu = (function() {
     var toggleMobileMenuBtns = document.querySelectorAll(".js-toggle-mobile-menu");
     var mobileMenu = document.querySelector(".js-mobile-menu");
 
-    for (i = 0; i < toggleMobileMenuBtns.length; i++) { 
+    var subMenuBtns = document.querySelectorAll(".menu-item-has-children > a");
+
+    for (i = 0; i < toggleMobileMenuBtns.length; i++) {
         toggleMobileMenuBtns[i].addEventListener("click", toggleMenu);
+    }
+    for (j = 0; j < subMenuBtns.length; j++) {
+        subMenuBtns[j].addEventListener("click", toggleSubMenu);
+        // If sub page is active, open and show sub menu
+        if (
+            subMenuBtns[j].parentElement.classList.contains("current-menu-ancestor") ||
+            subMenuBtns[j].parentElement.classList.contains("current-menu-parent") ||
+            subMenuBtns[j].parentElement.classList.contains("current-menu-item")
+        ) {
+            subMenuBtns[j].classList.add("open-toggle");
+            subMenuBtns[j].parentElement.querySelector(".sub-menu").classList.add("open");
+        }
     }
 
     function toggleMenu(e) {
         e.preventDefault();
         mobileMenu.classList.toggle("active");
+    }
+    function toggleSubMenu(e) {
+        e.preventDefault();
+        e.target.classList.toggle("open-toggle");
+        e.target.parentElement.querySelector(".sub-menu").classList.toggle("open");
     }
 })();
