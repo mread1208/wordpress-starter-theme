d48f9bf429b7fb7b3aa7470ae3030b226d7a11bf
Michael Read <michaelread1208@gmail.com>
2019-07-27 11:04:17 -0500

    Removed old bootstrap classes

--- footer.php
+++ footer.php
@@ -1,10 +1,6 @@
 	<footer id="footer">
 		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<p>&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?></p>
-				</div>
-			</div><?php // .row ?>
+			<p>&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?></p>
 		</div><?php // .container ?>
 	</footer><?php  //#footer ?>
 	<?php wp_footer(); ?>
